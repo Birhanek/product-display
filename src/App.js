73d84856c67b48b24d434003bce31bacc6cf90cb
Birhanek <birhane.kahsay99@gmail.com>
2023-02-23 11:25:20 +0100

    Adding get data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getAllProducts } from './components/ProductServices/ProductServices';
 
 function App() {
+
+  const [productsData,setProductsData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState([]);
+
+
+  const fetchData = async()=>{
+    const res = await getAllProducts();
+    try {
+      if(!res.ok){
+        throw new Error('Resource Note Found!');
+      }
+      setProductsData(res.json());
+      setIsLoading(false);
+      setError([]);
+    } catch (error) {
+      setProductsData([]);
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(()=>{
+    setIsLoading(true);
+    fetchData();
+  },[])
+
+
+const createElement ='';
+
+if(productsData.length > 0){
+  // createElement => creating the products component with data in productsData 
+  //as spread operator
+}
+if(productsData.length === 0 && error.length > 0){
+  // createElement => <p> error[0] </p>
+}
+if(isLoading){
+  // createElement => <p> Loading ....</p>
+}
+
   return (
     <div className="App">
-    welcome
+    {createElement}
     </div>
   );
 }
